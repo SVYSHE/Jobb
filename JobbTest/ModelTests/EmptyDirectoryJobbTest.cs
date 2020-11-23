c69151817575568804c52982bcb41d727b54c838
eldtt <knott0790@gmail.com>
2020-11-23 15:06:35 +0100

    created TestBase to handle temporary test data folder and cleanup

--- JobbTest/ModelTests/EmptyDirectoryJobbTest.cs
+++ JobbTest/ModelTests/EmptyDirectoryJobbTest.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using Jobb.Models.Implementations;
 using Jobb.Utility;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -5,25 +6,48 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace JobbTest.ModelTests
 {
     [TestClass]
-    public class EmptyDirectoryJobbTest
+    public class EmptyDirectoryJobbTest : TestBase
     {
+        private string emptyDirectory;
+        private readonly string testFile = "emptyTestFile.txt";
+
+        [TestInitialize]
+        public void Init() {
+            emptyDirectory = Path.Combine(LocalResourceTestDataPath, "EmptyDirectory");
+            CreateFolder(emptyDirectory);
+            CreateFile(Path.Combine(emptyDirectory, testFile));
+        }
+
+        [TestMethod]
+        public void ExecuteEmptyDirectoryJobbSuccess() {        
+            var deleteJob = new EmptyDirectoryJobb("TestJob", emptyDirectory).Execute();
+
+            Assert.AreEqual(JobbReturnCode.Success, deleteJob);
+        }
+
+        [TestMethod]
+        public void ExecuteEmptyDirectoryJobbError() {
+            var deleteJob = new EmptyDirectoryJobb("TestJob", "invalidFilePath").Execute();
+            Assert.AreEqual(JobbReturnCode.Error, deleteJob);
+        }
+
         [TestMethod]
         public void ReturnCodeIsSetToWaitingAfterConstructor()
         {
             const JobbReturnCode expected = JobbReturnCode.Waiting;
             
-            var deleteJob = new EmptyDirectoryJobb("TestJob", @"C:\"); 
+            var deleteJob = new EmptyDirectoryJobb("TestJob", emptyDirectory); 
             
             Assert.AreEqual(expected,deleteJob.ReturnCode);
         }
 
         [TestMethod]
-        public void getNameTest()
+        public void GetNameTest()
         {
             const string expected = "Testname";
             
-            var deleteJob = new EmptyDirectoryJobb("Testname", @"C:\");
-            
+            var deleteJob = new EmptyDirectoryJobb("Testname", emptyDirectory);
+
             Assert.AreEqual(expected, deleteJob.Name);
         }
     }
