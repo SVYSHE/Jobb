68fdea9723355bb61d51dd55acd976c8a650d344
eldtt <knott0790@gmail.com>
2020-12-08 08:58:07 +0100

    moved PeriodValueOutOfBoundsException message logic to Schedule

--- Jobb/Exceptions/PeriodValueOutOfBoundsException.cs
+++ Jobb/Exceptions/PeriodValueOutOfBoundsException.cs
@@ -5,16 +5,8 @@ namespace Jobb.Exceptions {
     [Serializable]
     public class PeriodValueOutOfBoundsException : Exception {
         public PeriodValueOutOfBoundsException() { }
-
         public PeriodValueOutOfBoundsException(string message) : base(message) { }
         public PeriodValueOutOfBoundsException(string message, Exception innerException) : base(message, innerException) { }
         protected PeriodValueOutOfBoundsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
-
-        public PeriodValueOutOfBoundsException(uint lowerBound, uint upperBound = 0) {
-            if (upperBound > 0)
-                throw new PeriodValueOutOfBoundsException($"Invalid value for Period. Value has to be between <{lowerBound + 1}> and <{upperBound - 1}>.");
-            else 
-                throw new PeriodValueOutOfBoundsException($"Invalid value for Period. Value has to be at least <{lowerBound + 1}>.");
-        }
     }
 }
--- Jobb/Models/Implementations/Schedule.cs
+++ Jobb/Models/Implementations/Schedule.cs
@@ -2,8 +2,7 @@
 using Jobb.Utility;
 
 namespace Jobb.Models.Implementations {
-    public class Schedule
-    {
+    public class Schedule {
         private const uint GeneralLowerBound = 0;
         private const uint SecondsUpperBound = 60;
         private const uint MinutesUpperBound = 60;
@@ -11,45 +10,45 @@ namespace Jobb.Models.Implementations {
         private const uint DaysUpperBound = 32;
         private const uint WeeksUpperBound = 52;
         private const uint MonthsUpperBound = 12;
-        
+
         private int _unit;
-        public int Unit
-        {
+        public int Unit {
             get => _unit;
-            set
-            {
-                _unit = Period switch
-                {
+            set {
+                _unit = Period switch {
                     Period.Seconds when value > GeneralLowerBound && value < SecondsUpperBound => value,
-                    Period.Seconds => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, SecondsUpperBound),
+                    Period.Seconds => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, SecondsUpperBound)),
                     Period.Minutes when value > GeneralLowerBound && value < MinutesUpperBound => value,
-                    Period.Minutes => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, MinutesUpperBound),
+                    Period.Minutes => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, MinutesUpperBound)),
                     Period.Hours when value > GeneralLowerBound && value < HoursUpperBound => value,
-                    Period.Hours => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, HoursUpperBound),
+                    Period.Hours => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, HoursUpperBound)),
                     Period.Days when value > GeneralLowerBound && value < DaysUpperBound => value,
-                    Period.Days => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, DaysUpperBound),
+                    Period.Days => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, DaysUpperBound)),
                     Period.Weeks when value > GeneralLowerBound && value < WeeksUpperBound => value,
-                    Period.Weeks => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, WeeksUpperBound),
+                    Period.Weeks => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, WeeksUpperBound)),
                     Period.Months when value > GeneralLowerBound && value < MonthsUpperBound => value,
-                    Period.Months => throw new PeriodValueOutOfBoundsException(GeneralLowerBound, MonthsUpperBound),
+                    Period.Months => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound, MonthsUpperBound)),
                     Period.Years when value > GeneralLowerBound => value,
-                    Period.Years => throw new PeriodValueOutOfBoundsException(GeneralLowerBound),
+                    Period.Years => throw new PeriodValueOutOfBoundsException(GetExceptionMessage(GeneralLowerBound)),
                     _ => throw new PeriodValueOutOfBoundsException("Please specify a matching period.")
                 };
-            } 
+            }
         }
         public Period Period { get; set; }
 
-        public Schedule()
-        {
+        public Schedule() {
             Period = Period.Seconds;
             Unit = 1;
         }
 
-        public Schedule(Period period, int unit)
-        {
+        public Schedule(Period period, int unit) {
             Period = period;
             Unit = unit;
+        }
+
+        private string GetExceptionMessage(uint lowerBound, uint upperBound = 0) {
+            return upperBound > 0 ? $"Invalid value for Period. Value has to be between <{lowerBound + 1}> and <{upperBound - 1}>."
+            : $"Invalid value for Period. Value has to be at least <{lowerBound + 1}>.";
         }
     }
 }
