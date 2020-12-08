85508795e4c78249c2a27e7a0ba1c531c53a6de3
eldtt <knott0790@gmail.com>
2020-12-08 08:25:18 +0100

    fix attempt

--- Jobb/Exceptions/PeriodValueOutOfBoundsException.cs
+++ Jobb/Exceptions/PeriodValueOutOfBoundsException.cs
@@ -10,9 +10,11 @@ namespace Jobb.Exceptions {
         public PeriodValueOutOfBoundsException(string message, Exception innerException) : base(message, innerException) { }
         protected PeriodValueOutOfBoundsException(SerializationInfo info, StreamingContext context) : base(info, context) { }
 
-        public PeriodValueOutOfBoundsException(uint lowerBound, uint upperBound = 0) : base(
-            upperBound > 0 ? string.Format($"Invalid value for Period. Value has to be between <{lowerBound + 1}> and <{upperBound - 1}>.")
-            : string.Format($"Invalid value for Period. Value has to be at least <{lowerBound + 1}>.")) { }
-
+        public PeriodValueOutOfBoundsException(uint lowerBound, uint upperBound = 0) {
+            if (upperBound > 0)
+                throw new PeriodValueOutOfBoundsException($"Invalid value for Period. Value has to be between <{lowerBound + 1}> and <{upperBound - 1}>.");
+            else 
+                throw new PeriodValueOutOfBoundsException($"Invalid value for Period. Value has to be at least <{lowerBound + 1}>.");
+        }
     }
 }
