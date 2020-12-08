e4612fae44020798f6e3cd3d9be572e8619d92ba
SVYSHE <rphp@web.de>
2020-12-08 14:18:37 +0100

    Added reference to https://github.com/migueldeicaza/gui.cs .

--- Jobb/Program.cs
+++ Jobb/Program.cs
@@ -1,7 +1,34 @@
-﻿namespace Jobb {
-    static class Program {
-        static void Main() {
-            // programm entry point
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks.Dataflow;
+using Terminal.Gui;
+
+namespace Jobb
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Application.Init();
+            var top = Application.Top;
+            
+            var win = new Window("MyApp")
+            {
+                X = 0,
+                Y = 1,
+
+                Width = Dim.Fill(),
+                Height = Dim.Fill(),
+            };
+            
+            IList dict = new List<string>();
+            dict.Add("Hello");
+            dict.Add("Goodbye");
+            
+            win.Add(new ListView(dict));
+            top.Add(win);
+
+            Application.Run();
         }
     }
 }
