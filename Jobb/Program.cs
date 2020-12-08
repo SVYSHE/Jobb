2a1bc7d137142702d956850ea409baf120fbe26a
eldtt <knott0790@gmail.com>
2020-12-08 11:14:53 +0100

    removed code from Main() method

--- Jobb/Program.cs
+++ Jobb/Program.cs
@@ -1,19 +1,7 @@
-﻿using Jobb.Models.Implementations;
-using Jobb.Models.Implementations.Jobbs;
-using Jobb.Utility;
-
-namespace Jobb {
+﻿namespace Jobb {
     static class Program {
         static void Main() {
-            var parameters = new EmptyDirectoryJobbParameters {
-                Name = "Test",
-                Schedule = new Schedule(Period.Seconds, 1),
-                TargetDirectory = @"C:\Test"
-            };
-            _ = new EmptyDirectoryJobb(parameters);
-            while (true) {
-                // test loop
-            }
+            // programm entry point
         }
     }
 }
