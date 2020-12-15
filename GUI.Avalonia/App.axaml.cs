282ad429b3ba6333d5c53818be2f8c504afae1b5
eldtt <knott0790@gmail.com>
2020-12-15 18:00:49 +0100

    converted WPF to Avalonia

---
+++ GUI.Avalonia/App.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls.ApplicationLifetimes;
+using Avalonia.Markup.Xaml;
+using GUI.Avalonia.ViewModels;
+using GUI.Avalonia.Views;
+
+namespace GUI.Avalonia {
+    public class App : Application {
+        public override void Initialize() {
+            AvaloniaXamlLoader.Load(this);
+        }
+
+        public override void OnFrameworkInitializationCompleted() {
+            if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop) {
+                desktop.MainWindow = new MainWindow {
+                    DataContext = new MainWindowViewModel(),
+                };
+            }
+
+            base.OnFrameworkInitializationCompleted();
+        }
+    }
+}
---
+++ GUI.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using ReactiveUI;
+
+namespace GUI.Avalonia.ViewModels {
+    public class ViewModelBase : ReactiveObject {
+        new public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
