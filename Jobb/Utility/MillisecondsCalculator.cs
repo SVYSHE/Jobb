27d9f55a4b7c8c8266317d297529f3c629fdd00a
SVYSHE <rphp@web.de>
2020-12-16 16:22:38 +0100

    Added more tests.

--- Jobb/Utility/MillisecondsCalculator.cs
+++ Jobb/Utility/MillisecondsCalculator.cs
@@ -5,25 +5,25 @@ namespace Jobb.Utility
 {
     public static class MillisecondsCalculator
     {
-        private const ulong SecondInMilliseconds = 1_000;
-        private const ulong MinuteInMilliseconds = 60_000;
-        private const ulong HourInMilliseconds = 3_600_000;
-        private const ulong DayInMilliseconds = 86_400_000;
-        private const ulong WeekInMilliseconds = 604_800_016;
-        private const ulong MonthInMilliseconds = 2_629_800_000;
-        private const ulong YearInMilliseconds = 31_557_600_000;
+        private const ulong SECOND_IN_MILLISECONDS = 1_000;
+        private const ulong MINUTE_IN_MILLISECONDS = 60_000;
+        private const ulong HOUR_IN_MILLISECONDS = 3_600_000;
+        private const ulong DAY_IN_MILLISECONDS = 86_400_000;
+        private const ulong WEEK_IN_MILLISECONDS = 604_800_016;
+        private const ulong MONTH_IN_MILLISECONDS = 2_629_800_000;
+        private const ulong YEAR_IN_MILLISECONDS = 31_557_600_000;
 
         public static ulong GetMilliseconds(Schedule schedule)
         {
             return schedule.Period switch
             {
-                Period.Seconds => SecondInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Minutes => MinuteInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Hours => HourInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Days => DayInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Weeks => WeekInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Months => MonthInMilliseconds * Convert.ToUInt64(schedule.Unit),
-                Period.Years => YearInMilliseconds * Convert.ToUInt64(schedule.Unit),
+                Period.Seconds => SECOND_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Minutes => MINUTE_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Hours => HOUR_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Days => DAY_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Weeks => WEEK_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Months => MONTH_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
+                Period.Years => YEAR_IN_MILLISECONDS * Convert.ToUInt64(schedule.Unit),
                 _ => 0
             };
         }
