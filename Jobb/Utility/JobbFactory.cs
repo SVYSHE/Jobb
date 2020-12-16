cb638f3f10480512255e3acca41cedbd67033cb0
SVYSHE <rphp@web.de>
2020-12-16 15:31:42 +0100

    BugFix in JobbFactory after adding unit tests.

--- Jobb/Utility/JobbFactory.cs
+++ Jobb/Utility/JobbFactory.cs
@@ -7,7 +7,7 @@ using static Jobb.Utility.ScheduleUtility;
 
 namespace Jobb.Utility
 {
-    public class JobbFactory
+    public static class JobbFactory
     {
         public static AbstractJobb GetJobb(JobbType jobbType, params object[] parameterValues)
         {
@@ -17,7 +17,6 @@ namespace Jobb.Utility
                 JobbType.EmptyDirectory => CreateEmptyDirectoryJobb(parameterValues),
                 _ => throw new InvalidOperationException()
             };
-            SetAbstractParameters(parameterValues, jobbToReturn);
             return jobbToReturn;
         }
 
@@ -25,7 +24,7 @@ namespace Jobb.Utility
         {
             var jobbParameter = new List<string>();
             jobbParameter = AddAbstractParameter(jobbParameter);
-            
+
             return jobbType switch
             {
                 JobbType.CopyFile => AddCopyFileParameter(jobbParameter),
@@ -56,27 +55,29 @@ namespace Jobb.Utility
 
         private static EmptyDirectoryJobb CreateEmptyDirectoryJobb(IReadOnlyList<object> parameterValues)
         {
-            var jobb = new EmptyDirectoryJobb(new EmptyDirectoryJobbParameters());
-            jobb.Parameters.TargetDirectory = parameterValues[3].ToString();
-            return jobb;
+            var parameters = new EmptyDirectoryJobbParameters();
+            parameters = (EmptyDirectoryJobbParameters) SetAbstractParameters(parameterValues, parameters);
+            parameters.TargetDirectory = parameterValues[3].ToString();
+            return new EmptyDirectoryJobb(parameters);
         }
 
         private static CopyFileJobb CreateCopyFileJobb(IReadOnlyList<object> parameterValues)
         {
-            var jobb = new CopyFileJobb(new CopyFileJobbParameters());
-            jobb.Parameters.SourceDirectory = parameterValues[3].ToString();
-            jobb.Parameters.TargetDirectory = parameterValues[4].ToString();
-            jobb.Parameters.FileName = parameterValues[5].ToString();
-            return jobb;
+            var parameters = new CopyFileJobbParameters();
+            parameters = (CopyFileJobbParameters) SetAbstractParameters(parameterValues, parameters);
+            parameters.SourceDirectory = parameterValues[3].ToString();
+            parameters.TargetDirectory = parameterValues[4].ToString();
+            parameters.FileName = parameterValues[5].ToString();
+            return new CopyFileJobb(parameters);
         }
 
-        private static void SetAbstractParameters(IReadOnlyList<object> parameterValues, AbstractJobb jobbToReturn)
+        private static AbstractJobbParameters SetAbstractParameters(IReadOnlyList<object> parameterValues,
+            AbstractJobbParameters parameters)
         {
-            jobbToReturn.Parameters.Name = parameterValues[0].ToString();
-            jobbToReturn.Parameters.Schedule =
+            parameters.Name = parameterValues[0].ToString();
+            parameters.Schedule =
                 GetScheduleFromString(parameterValues[1].ToString(), parameterValues[2].ToString());
+            return parameters;
         }
-        
-        
     }
 }
