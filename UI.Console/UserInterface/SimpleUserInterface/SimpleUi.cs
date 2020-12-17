10a249f13dffad304fe886fac00ef384e2e7d09e
SVYSHE <rphp@web.de>
2020-12-17 09:38:15 +0100

    Refactoring.

--- UI.Console/UserInterface/SimpleUserInterface/SimpleUi.cs
+++ UI.Console/UserInterface/SimpleUserInterface/SimpleUi.cs
@@ -5,21 +5,21 @@ namespace UserInterface.SimpleUserInterface
     /// <summary>
     /// This class is a way to output to the user at first glance.
     /// </summary>
-    public class SimpleUi
+    public static class SimpleUi
     {
-        public string Print(string text)
+        public static string Print(string text)
         {
             Console.WriteLine(text);
             return text;
         }
 
-        public string PrintWithUserFullfillment(string text)
+        public static string PrintWithUserFullfillment(string text)
         {
             Console.Write(text);
             return text;
         }
 
-        public string PrintWithColor(string text, ConsoleColor color)
+        public static string PrintWithColor(string text, ConsoleColor color)
         {
             Console.ForegroundColor = color;
             Console.WriteLine(text);
@@ -27,14 +27,14 @@ namespace UserInterface.SimpleUserInterface
             return text;
         }
 
-        public string PrintWithTimestamp(string text)
+        public static string PrintWithTimestamp(string text)
         {
             string output = $"[{DateTime.Now}] {text}";
             Console.WriteLine(output);
             return output;
         }
 
-        public string PrintWithTimestampAndColor(string text, ConsoleColor color)
+        public static string PrintWithTimestampAndColor(string text, ConsoleColor color)
         {
             string output = $"[{DateTime.Now}] {text}";
             Console.ForegroundColor = color;
@@ -43,12 +43,12 @@ namespace UserInterface.SimpleUserInterface
             return output;
         }
 
-        public string ReadLine()
+        public static string ReadLine()
         {
             return Console.ReadLine();
         }
 
-        public bool ParseYesNo(string text)
+        public static bool ParseYesNo(string text)
         {
             text = text.ToLower();
             if (text.Equals("y") || text.Equals("yes"))
